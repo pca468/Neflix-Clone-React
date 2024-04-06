527a9790fc0f426e5acc5ccf031587bb4bd8cadc
DongHyun <pca468@naver.com>
2024-04-06 23:01:35 +0900

    first Commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import AppLayout from './layout/AppLayout';
+import HomePage from './pages/Homepage/HomePage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundpage/NotFoundPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+// 홈페이지 /
+// 영화 전체 보여주는 페이지(서치) /movies
+// 영화 디테일 페이지  /movies/:id
+// 추천영화 / movies/:id/recommandation
+// 리부 /movie:id/reviews
+
+function App() {
+  return ( // AppLayout으로 Route를 묶어줘야 같은 Layout을 보여줄 수 있다.
+  // index는 위에 부모 path와 동일하게 사용하겠다 라는 뜻
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element = {<HomePage />}/>
+          <Route path='movies'>
+            <Route index element={<MoviePage />} />
+            <Route path=':id' element={<MovieDetailPage />} />
+          </Route>
+          {/* <Route path='/movies' element={<MoviePage />}/>
+          <Route  path='/movies/:id' element={<MovieDetailPage />}/> */}
+        </Route>
+
+        <Route path='*' element={<NotFoundPage />}/>
+
+      </Routes>
+ 
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Outlet } from "react-router-dom"
+
+//Outlet =>  라우터 안에 있는 자손들을 갖고 오게 도와주는 컴포넌트
+
+const AppLayout = () => { 
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-dark">
+        <Container fluid >
+          <Navbar.Brand href="#" variant>
+            <img 
+            src="//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png"
+            alt="Neflix logo"
+            width={100}
+            style={{marginLeft:20}}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" , }}
+              navbarScroll
+            >
+              <Nav.Link href="/" style={{color: "white"}}>Home</Nav.Link>
+              <Nav.Link href="/movies" style={{color: "white"}}>Movies</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;
