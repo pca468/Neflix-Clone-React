37a2a08159f38b178950a27a06271a56e62fa43e
DongHyun <pca468@naver.com>
2024-04-11 02:34:33 +0900

    pagenation add

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchMoviesQuery } from "../../hooks/useSearchMovie";
 import { useSearchParams } from "react-router-dom";
 import { Container, Alert, Row, Col } from "react-bootstrap";
 import MovieCard from "../../common/MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
 
 // 경로 2가지
 //nav바에서 클릭해 온 경우 => populartMovie 보여주기
 //keyword를 입력해서 온 경우 => keyword와 관련된 영화들을 보여줌
 
+//페이지네이션 설치
+// page state 만들기
+// 페이지네이션 클릭 할 때 마다 page 바꿔주기
+// page 값이 바뀔 때 마다 useSearchMovie에 page까지 넣어서 fetch
+
 const MoviePage = () => {
   const [query, setQuery] = useSearchParams();
+  const [page, setPage] = useState(1);
   const keyword = query.get("q");
 
-  const { data, isLodaing, isError, error } = useSearchMoviesQuery({ keyword });
+  const { data, isLodaing, isError, error } = useSearchMoviesQuery({ keyword, page });
+
+  const handlePageClick = ({selected}) => {
+    setPage(selected + 1)
+  };
+  console.log("asd",data)
 
   if (isLodaing) {
     return <h1>Loading...</h1>;
@@ -35,6 +47,27 @@ const MoviePage = () => {
               </Col>
             ))}
           </Row>
+          <ReactPaginate
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={2}
+            pageCount={data?.total_pages} // 전체 페이지가 몇개인지
+            previousLabel="< previous"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+            forcePage={page - 1}
+          />
         </Col>
       </Row>
     </Container>
